c139a54df570b3dbd87fdd865d3a71efaa49c16b
celsojunio11 <celsojunio11@gmail.com>
2021-05-27 21:59:59 -0300

    adicionandos views, alteração funcionarios.model e funcionarioscontroller

--- controllers/funcionariosController.js
+++ controllers/funcionariosController.js
@@ -1,8 +1,37 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Funcionarios = mongoose.model('Funcionarios');
+
 
 router.get('/', (req, res) => {
-  res.json('Texto teste');
+  res.render("funcionariosGrid/addOrEdit", {
+    viewTitle : "Inserir Funcionários da SpaceX"
+  });
 });
+
+router.post('/', (req, res) => {
+    insertRecord(req, res);
+});
+
+function insertRecord(req, res) {
+  var funcionarios = new Funcionarios();
+  funcionarios.nome = req.body.nome;
+  funcionarios.email = req.body.email;
+  funcionarios.nacionalidade = req.body.nacionalidade;
+  funcionarios.cidade = req.body.cidade;
+  funcionarios.telefone = req.body.telefone;
+  funcionarios.save((err, doc) => {
+    if (!err) {
+      res.redirect('funcionarios/list');
+    }else{
+      console.log('Erro durante a inserção : ' + err);
+    }
+  });
+}
+
+router.get('/list', (req, res) => {
+    res.json('from list');
+ });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,15 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
 
 const funcionariosController = require('./controllers/funcionariosController');
 
 var app = express();
-
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
